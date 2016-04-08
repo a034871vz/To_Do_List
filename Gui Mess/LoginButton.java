89bccd48410334a7cf1b1f40d86122276a7b995b
a034871vz <a034871vz@gmail.com>
2016-04-08 08:58:28 +0300

    делать

---
+++ Gui Mess/LoginButton.java
@@ -0,0 +1,13 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginButton implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		
+		System.out.println("Кнопка нажата!");
+		
+	}
+
+}
